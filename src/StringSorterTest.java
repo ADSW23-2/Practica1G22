d8536f7ae519f13c71254d9f369c3d7103ea4703
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2017-02-19 00:56:55 +0100

    Test arreglado, ya no da por culo el mergesort Arreglado problema merge sort que no ordena numeros repetidos

--- src/StringSorterTest.java
+++ src/StringSorterTest.java
@@ -70,33 +70,21 @@ public class StringSorterTest {
         carga3d = desordenar(carga3);
         carga4d = desordenar(carga4);
         carga5d = desordenar(carga5);
-
         cargaConEnied = desordenar(cargaConEnie);
-
-
         cargaConNumerosd = desordenar(cargaConNumeros);
-
-        cargaConSimbolosd = desordenar(cargaConSimbolos);
-
+        //cargaConSimbolosd = desordenar(cargaConSimbolos);
 
 
-        //TODO: VER POR QUE COJONES NO SE PASA EL VALOR AL ARRAY
-        sorterInsertion.sort(carga1d);
+        //YA TA ARREGLADO
+        //TODO --  VER POR QUE COJONES NO SE PASA EL VALOR AL ARRAY
+        sorterMerge.sort(carga1d);
         sorterMerge.sort(carga2d);
-        carga2d = sorterMerge.getArrayOrdenado();
         sorterMerge.sort(carga3d);
-        carga3d = sorterMerge.getArrayOrdenado();
         sorterMerge.sort(carga4d);
-        carga4d = sorterMerge.getArrayOrdenado();
         sorterMerge.sort(carga5d);
-        carga5d = sorterMerge.getArrayOrdenado();
         sorterMerge.sort(cargaConEnied);
-        cargaConEnied = sorterMerge.getArrayOrdenado();
         sorterMerge.sort(cargaConNumerosd);
-        cargaConNumerosd = sorterMerge.getArrayOrdenado();
-        //sorterMerge.sort(cargaConSimbolosd);
-        //printer(cargaConSimbolos);
-        //printer(cargaConSimbolosd);
+
 
 
         assertArrayEquals(carga1,carga1d);
